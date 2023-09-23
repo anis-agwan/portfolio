9a81601501c5ebe97c37e55b24b2d9097505b303
Mohmed Anis Agwan <anishagwan@gmail.com>
2023-09-22 20:27:40 -0400

    Updated skills section

--- src/pages/skills/skills.data.js
+++ src/pages/skills/skills.data.js
@@ -1,192 +1,413 @@
-import L_HTML5 from '../../assets/img/skills/html-5.svg';
-import L_CSS3 from '../../assets/img/skills/css3.svg';
-import L_BOOTSTRAP from '../../assets/img/skills/bootstrap-4.svg';
-import L_REACT from '../../assets/img/skills/react.svg';
-import L_REDUX from '../../assets/img/skills/redux.svg';
-import L_FLUTTER from '../../assets/img/skills/flutter.svg';
-import L_REACT_BOOTSTRAP from '../../assets/img/skills/react-bootstrap.svg';
-import L_SEMANTIC from '../../assets/img/skills/semantic-ui.svg';
-import L_NODE_JS from '../../assets/img/skills/nodejs.svg';
-import L_DJANGO from '../../assets/img/skills/django.svg';
-import L_FLASK from '../../assets/img/skills/flask.svg';
-import L_DART from '../../assets/img/skills/dart.svg';
-import L_ML from '../../assets/img/skills/ml.svg';
-import L_DL from '../../assets/img/skills/deep-learning.svg';
-import L_Pytorch from '../../assets/img/skills/PyTorch.svg';
-import L_TF from '../../assets/img/skills/Tensorflow.svg';
-import L_SCIKIT from '../../assets/img/skills/scikit.svg';
-import L_KERAS from '../../assets/img/skills/Keras.svg';
-import L_JAVASCRIPT from '../../assets/img/skills/javascript.svg';
-import L_JAVA from '../../assets/img/skills/icons8-java.svg';
-import L_SQL from '../../assets/img/skills/sql.svg';
-import L_PYTHON from '../../assets/img/skills/python.svg';
-import L_SWIFT from '../../assets/img/skills/swift.svg';
-import L_JUPYTER from '../../assets/img/skills/jupyter.svg';
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import L_REDUX from "../../assets/img/skills/redux.svg";
+import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import L_SEMANTIC from "../../assets/img/skills/semantic-ui.svg";
+// import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
+import L_DJANGO from "../../assets/img/skills/django.svg";
+import L_FLASK from "../../assets/img/skills/flask.svg";
+import L_ML from "../../assets/img/skills/ml.svg";
+import L_Pytorch from "../../assets/img/skills/PyTorch.svg";
+// import L_TF from "../../assets/img/skills/Tensorflow.svg";
+// import L_SCIKIT from '../../assets/img/skills/scikit.svg';
+import L_KERAS from "../../assets/img/skills/Keras.svg";
 
-import L_POSTMAN from '../../assets/img/skills/postman.svg';
-import L_MONGODB from '../../assets/img/skills/mongodb.svg';
-import L_ORACLE from '../../assets/img/skills/oracle.svg';
-import L_POSTGRESQL from '../../assets/img/skills/postgresql.svg';
-import L_GIT from '../../assets/img/skills/git-icon.svg';
-import L_XCODE from '../../assets/img/skills/xcode.png';
-import L_SWIFTUI from '../../assets/img/skills/swiftUI.png';
+//Languages
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+import L_TS from "../../assets/img/skills/typescript-icon.svg";
+import L_JAVA from "../../assets/img/skills/icons8-java.svg";
+import L_CPP from "../../assets/img/skills/icons8-c++-48.svg";
+import L_SQL from "../../assets/img/skills/sql.svg";
+import L_GRAPH from "../../assets/img/skills/graphql-icon.svg";
+import L_PYTHON from "../../assets/img/skills/python.svg";
+import L_SWIFT from "../../assets/img/skills/swift.svg";
+import L_OBJC from "../../assets/img/skills/apple_objectivec-ar21.svg";
+import L_GO from "../../assets/img/skills/go-icon.svg";
+// import L_JUPYTER from "../../assets/img/skills/jupyter.svg";
+
+//Frameworks and libraries
+import L_IOS from "../../assets/img/skills/ios-icon.svg";
+import L_SWIFTUI from "../../assets/img/skills/swiftUI.png";
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_NODE from "../../assets/img/skills/icons8-nodejs.svg";
+import L_SPRING from "../../assets/img/skills/springio-icon.svg";
+
+import L_POSTMAN from "../../assets/img/skills/postman.svg";
+
+//Databases
+import L_MONGODB from "../../assets/img/skills/mongodb.svg";
+import L_ORACLE from "../../assets/img/skills/oracle.svg";
+import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
+import L_MYSQL from "../../assets/img/skills/mysql-official.svg";
+import L_CASS from "../../assets/img/skills/apache_cassandra-ar21.svg";
+import L_DYNAMO from "../../assets/img/skills/aws-dynamodb.svg";
+import L_FIREBASE from "../../assets/img/skills/firebase-icon.svg";
+
+//Cloud
+import L_AWS from "../../assets/img/skills/aws-2.svg";
+import L_GCP from "../../assets/img/skills/google-cloud-1.svg";
+
+//Devops
+import L_GIT from "../../assets/img/skills/git-icon.svg";
+import L_DOCKER from "../../assets/img/skills/docker-svgrepo-com.svg";
+import L_KUBERNETES from "../../assets/img/skills/kubernetes-icon.svg";
+import L_GITHUB from "../../assets/img/skills/icons8-github.svg";
+
+//IDEs
+import L_XCODE from "../../assets/img/skills/xcode.png";
+import L_IDEA from "../../assets/img/skills/intellij-idea-svgrepo-com.svg";
+import L_ANDSTUDIO from "../../assets/img/skills/android-studio-canary.svg";
+import L_VSCODE from "../../assets/img/skills/Visual_Studio_Code_1.35_icon.svg.png";
+import L_PYCHARM from "../../assets/img/skills/PyCharm_Icon.svg.png";
+
+import L_WORKBENCH from "../../assets/img/skills/512px-Antu_mysql-workbench.svg.png";
 
 export const skills = {
+  languages: [
+    {
+      link: "https://www.java.com/en/",
+      imgAltText: "Java",
+      imgSrc: L_JAVA,
+      skillName: "Java",
+    },
+    {
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    },
+    {
+      link: "https://www.typescriptlang.org/",
+      imgAltText: "TypeScript",
+      imgSrc: L_TS,
+      skillName: "TypeScript",
+    },
+    {
+      link: "https://developer.apple.com/swift/",
+      imgAltText: "Swift",
+      imgSrc: L_SWIFT,
+      skillName: "Swift",
+    },
+    {
+      link: "https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/Introduction/Introduction.html",
+      imgAltText: "Obj-C",
+      imgSrc: L_OBJC,
+      skillName: "Objective C",
+    },
+    {
+      link: "https://cplusplus.com/",
+      imgAltText: "C++",
+      imgSrc: L_CPP,
+      skillName: "C++",
+    },
+    {
+      link: "https://www.python.org/",
+      imgAltText: "Python",
+      imgSrc: L_PYTHON,
+      skillName: "Python",
+    },
+    {
+      link: "https://go.dev/",
+      imgAltText: "Go",
+      imgSrc: L_GO,
+      skillName: "Go",
+    },
+    {
+      link: "https://en.wikipedia.org/wiki/HTML5",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link: "https://www.tutorialspoint.com/css/css3_tutorial.htm",
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
+    },
+
+    {
+      link: "https://www.w3schools.com/sql/",
+      imgAltText: "SQL",
+      imgSrc: L_SQL,
+      skillName: "SQL",
+    },
+    {
+      link: "https://graphql.org/",
+      imgAltText: "GraphQL",
+      imgSrc: L_GRAPH,
+      skillName: "GraphQL",
+    },
+  ],
+  frameworks: [
+    {
+      link: "https://www.apple.com/ios/ios-17/",
+      imgAltText: "iOS",
+      imgSrc: L_IOS,
+      skillName: "iOS",
+    },
+    {
+      link: "https://developer.apple.com/xcode/swiftui/",
+      imgAltText: "SwiftUI",
+      imgSrc: L_SWIFTUI,
+      skillName: "SwiftUI",
+    },
+    {
+      link: "https://reactjs.org/",
+      imgAltText: "React JS",
+      imgSrc: L_REACT,
+      skillName: "React JS",
+    },
+    {
+      link: "https://redux.js.org/",
+      imgAltText: "Redux",
+      imgSrc: L_REDUX,
+      skillName: "Redux",
+    },
+    {
+      link: "https://nodejs.org/en",
+      imgAltText: "NodeJS",
+      imgSrc: L_NODE,
+      skillName: "NodeJS",
+    },
+    {
+      link: "https://www.djangoproject.com/",
+      imgAltText: "Django",
+      imgSrc: L_DJANGO,
+      skillName: "Django",
+    },
+    {
+      link: "https://flask.palletsprojects.com/",
+      imgAltText: "Flask",
+      imgSrc: L_FLASK,
+      skillName: "Flask",
+    },
+    {
+      link: "https://spring.io/projects/spring-boot",
+      imgAltText: "Sprint Boot",
+      imgSrc: L_SPRING,
+      skillName: "Spring Boot",
+    },
+  ],
   frontend: [
     {
-      link: 'https://en.wikipedia.org/wiki/HTML5',
-      imgAltText: 'HTML 5',
+      link: "https://www.apple.com/ios/ios-17/",
+      imgAltText: "iOS",
       imgSrc: L_HTML5,
-      skillName: 'HTML5',
+      skillName: "iOS",
     },
     {
-      link: 'https://www.tutorialspoint.com/css/css3_tutorial.htm',
-      imgAltText: 'CSS 3',
+      link: "https://en.wikipedia.org/wiki/HTML5",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link: "https://www.tutorialspoint.com/css/css3_tutorial.htm",
+      imgAltText: "CSS 3",
       imgSrc: L_CSS3,
-      skillName: 'CSS3',
+      skillName: "CSS3",
     },
     {
-      link: 'https://www.javascript.com/',
-      imgAltText: 'JavaScript',
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
       imgSrc: L_JAVASCRIPT,
-      skillName: 'JavaScript',
+      skillName: "JavaScript",
     },
     {
-      link: 'https://getbootstrap.com/',
-      imgAltText: 'Bootstrap',
+      link: "https://getbootstrap.com/",
+      imgAltText: "Bootstrap",
       imgSrc: L_BOOTSTRAP,
-      skillName: 'Bootstrap',
+      skillName: "Bootstrap",
     },
     {
-      link: 'https://reactjs.org/',
-      imgAltText: 'React JS',
+      link: "https://reactjs.org/",
+      imgAltText: "React JS",
       imgSrc: L_REACT,
-      skillName: 'React JS',
+      skillName: "React JS",
     },
     {
-      link: 'https://redux.js.org/',
-      imgAltText: 'Redux',
+      link: "https://redux.js.org/",
+      imgAltText: "Redux",
       imgSrc: L_REDUX,
-      skillName: 'Redux',
+      skillName: "Redux",
     },
     {
-      link: 'https://react-bootstrap.github.io/',
-      imgAltText: 'React Bootstrap',
+      link: "https://react-bootstrap.github.io/",
+      imgAltText: "React Bootstrap",
       imgSrc: L_REACT_BOOTSTRAP,
-      skillName: 'React Bootstrap',
+      skillName: "React Bootstrap",
     },
     {
-      link: 'https://react.semantic-ui.com/',
-      imgAltText: 'Semantic UI',
+      link: "https://react.semantic-ui.com/",
+      imgAltText: "Semantic UI",
       imgSrc: L_SEMANTIC,
-      skillName: 'Semantic UI',
+      skillName: "Semantic UI",
     },
   ],
   backend: [
     {
-      link: 'https://www.djangoproject.com/',
-      imgAltText: 'Django',
+      link: "https://www.djangoproject.com/",
+      imgAltText: "Django",
       imgSrc: L_DJANGO,
-      skillName: 'Django',
+      skillName: "Django",
     },
     {
-      link: 'https://flask.palletsprojects.com/',
-      imgAltText: 'Flask',
+      link: "https://flask.palletsprojects.com/",
+      imgAltText: "Flask",
       imgSrc: L_FLASK,
-      skillName: 'Flask',
+      skillName: "Flask",
     },
   ],
   database: [
     {
-      link: 'https://www.postgresql.org/',
-      imgAltText: 'PostgreSQL',
+      link: "https://www.postgresql.org/",
+      imgAltText: "PostgreSQL",
       imgSrc: L_POSTGRESQL,
-      skillName: 'PostgreSQL',
+      skillName: "PostgreSQL",
     },
     {
-      link: 'https://www.mongodb.com/',
-      imgAltText: 'MongoDB',
+      link: "https://www.mongodb.com/",
+      imgAltText: "MongoDB",
       imgSrc: L_MONGODB,
-      skillName: 'MongoDB',
+      skillName: "MongoDB",
     },
     {
-      link: 'https://www.oracle.com/database/',
-      imgAltText: 'Oracle Database',
+      link: "https://www.oracle.com/database/",
+      imgAltText: "Oracle Database",
       imgSrc: L_ORACLE,
-      skillName: 'Oracle Database',
+      skillName: "Oracle Database",
+    },
+    {
+      link: "https://www.mysql.com/",
+      imgAltText: "MySQL",
+      imgSrc: L_MYSQL,
+      skillName: "MySQL",
+    },
+    {
+      link: "https://cassandra.apache.org/_/index.html",
+      imgAltText: "Apache Cassandra",
+      imgSrc: L_CASS,
+      skillName: "Apache Cassandra",
+    },
+    {
+      link: "https://aws.amazon.com/dynamodb/",
+      imgAltText: "DynamoDB",
+      imgSrc: L_DYNAMO,
+      skillName: "DynamoDB",
+    },
+    {
+      link: "https://developer.apple.com/documentation/coredata/",
+      imgAltText: "Core Data",
+      imgSrc: L_IOS,
+      skillName: "Core Data",
+    },
+    {
+      link: "https://firebase.google.com/docs/firestore",
+      imgAltText: "Firestore",
+      imgSrc: L_FIREBASE,
+      skillName: "Firestore",
     },
   ],
-  programmingLanguages: [
+  cloud: [
     {
-      link: 'https://www.python.org/',
-      imgAltText: 'Python',
-      imgSrc: L_PYTHON,
-      skillName: 'Python',
+      link: "https://aws.amazon.com/",
+      imgAltText: "AWS",
+      imgSrc: L_AWS,
+      skillName: "AWS",
     },
     {
-      link: 'https://developer.apple.com/swift/',
-      imgAltText: 'Swift',
-      imgSrc: L_SWIFT,
-      skillName: 'Swift',
+      link: "https://cloud.google.com/",
+      imgAltText: "GCP",
+      imgSrc: L_GCP,
+      skillName: "Google Cloud Platform",
+    },
+  ],
+  devops: [
+    {
+      link: "https://git-scm.com/",
+      imgAltText: "GIT",
+      imgSrc: L_GIT,
+      skillName: "GIT",
     },
     {
-      link: 'https://www.javascript.com/',
-      imgAltText: 'JavaScript',
-      imgSrc: L_JAVASCRIPT,
-      skillName: 'JavaScript',
+      link: "https://www.docker.com/",
+      imgAltText: "DOCKER",
+      imgSrc: L_DOCKER,
+      skillName: "Docker",
     },
     {
-      link: 'https://www.java.com/en/',
-      imgAltText: 'Java',
-      imgSrc: L_JAVA,
-      skillName: 'Java',
+      link: "https://kubernetes.io/",
+      imgAltText: "KUBER",
+      imgSrc: L_KUBERNETES,
+      skillName: "Kubernetes",
     },
     {
-      link: 'https://www.w3schools.com/sql/',
-      imgAltText: 'SQL',
-      imgSrc: L_SQL,
-      skillName: 'SQL',
+      link: "https://github.com/",
+      imgAltText: "GITHUB",
+      imgSrc: L_GITHUB,
+      skillName: "GitHub",
+    },
+  ],
+  ides: [
+    {
+      link: "https://developer.apple.com/xcode/",
+      imgAltText: "XCODE",
+      imgSrc: L_XCODE,
+      skillName: "Xcode",
+    },
+    {
+      link: "https://www.jetbrains.com/idea/",
+      imgAltText: "IDEA",
+      imgSrc: L_IDEA,
+      skillName: "IntelliJ IDEA",
+    },
+    {
+      link: "https://developer.android.com/studio/",
+      imgAltText: "STUDIO",
+      imgSrc: L_ANDSTUDIO,
+      skillName: "Android Studio",
+    },
+    {
+      link: "https://code.visualstudio.com/",
+      imgAltText: "VSCODE",
+      imgSrc: L_VSCODE,
+      skillName: "Visual Studio Code",
+    },
+    {
+      link: "https://www.jetbrains.com/pycharm/",
+      imgAltText: "PyCharm",
+      imgSrc: L_PYCHARM,
+      skillName: "PyCharm",
     },
   ],
   otherTools: [
     {
-      link: 'https://git-scm.com/',
-      imgAltText: 'GIT',
-      imgSrc: L_GIT,
-      skillName: 'GIT',
-    },
-    {
-      link: 'https://developer.apple.com/xcode/',
-      imgAltText: 'XCODE',
-      imgSrc: L_XCODE,
-      skillName: 'Xcode',
-    },
-    {
-      link: 'https://www.postman.com/',
-      imgAltText: 'POSTMAN',
+      link: "https://www.postman.com/",
+      imgAltText: "POSTMAN",
       imgSrc: L_POSTMAN,
-      skillName: 'Postman',
+      skillName: "Postman",
     },
     {
-      link: 'https://jupyter.org/',
-      imgAltText: 'Jupyter',
-      imgSrc: L_JUPYTER,
-      skillName: 'Jupyter Notebook',
+      link: "https://www.mysql.com/products/workbench/",
+      imgAltText: "MySQLWork",
+      imgSrc: L_WORKBENCH,
+      skillName: "MySQL Workbench",
     },
   ],
 
   ML: [
     {
-      link: 'https://developer.apple.com/xcode/swiftui/',
-      imgAltText: 'SwiftUI',
+      link: "https://developer.apple.com/xcode/swiftui/",
+      imgAltText: "SwiftUI",
       imgSrc: L_SWIFTUI,
-      skillName: 'SwiftUI',
+      skillName: "SwiftUI",
     },
     {
-      link: 'https://en.wikipedia.org/wiki/Machine_learning',
-      imgAltText: 'Machine Learning',
+      link: "https://en.wikipedia.org/wiki/Machine_learning",
+      imgAltText: "Machine Learning",
       imgSrc: L_ML,
-      skillName: 'Machine Learning',
+      skillName: "Machine Learning",
     },
     // {
     //   link: 'https://machinelearningmastery.com/what-is-deep-learning/',
@@ -194,23 +415,23 @@ export const skills = {
     //   imgSrc: L_DL,
     //   skillName: 'Deep Learning',
     // },
+    // {
+    //   link: "https://scikit-learn.org/",
+    //   imgAltText: "Scikit-learn",
+    //   imgSrc: L_SCIKIT,
+    //   skillName: "Scikit-learn",
+    // },
     {
-      link: 'https://scikit-learn.org/',
-      imgAltText: 'Scikit-learn',
-      imgSrc: L_SCIKIT,
-      skillName: 'Scikit-learn',
-    },
-    {
-      link: 'https://pytorch.org/',
-      imgAltText: 'PyTorch',
+      link: "https://pytorch.org/",
+      imgAltText: "PyTorch",
       imgSrc: L_Pytorch,
-      skillName: 'PyTorch',
+      skillName: "PyTorch",
     },
     {
-      link: 'https://keras.io/',
-      imgAltText: 'Keras',
+      link: "https://keras.io/",
+      imgAltText: "Keras",
       imgSrc: L_KERAS,
-      skillName: 'Keras',
+      skillName: "Keras",
     },
   ],
 };
